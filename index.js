055188ea9b7846d2773fec78c6c3e9eca46af0a3
Whyusernamelol <151388456+Whyusernamelol@users.noreply.github.com>
2024-10-16 13:00:43 +0545

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["JUST TESTING", "🎮 Playing VALORANT"];
+const statusMessages = ["DM ME TO CLAIM YOUR REWARD"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
